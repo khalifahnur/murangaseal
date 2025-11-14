f88f48aa9ed451009b817e4d7f4528c6cf9868c2
khalifahnur <khalifahnur1095@gmail.com>
2025-11-14 17:20:57 +0300

    fix:component to be dynamic

--- src/components/Home/HeroSecction.tsx
+++ src/components/Home/HeroSecction.tsx
@@ -1,5 +1,26 @@
+// import { getPayloadClient } from "@/lib/payloadClient";
+// import News from "./News";
+
 import { getPayloadClient } from "@/lib/payloadClient";
 import News from "./News";
+
+// export default async function HeroSection() {
+//   const payload = await getPayloadClient();
+
+//   const { docs: news } = await payload.find({
+//     collection: "news",
+//     sort: "-publishDate",
+//     limit: 3,
+//     depth: 1,
+//   });
+
+//   if (!news || news.length === 0) {
+//     // return <div className="py-20 text-center">No news yet. Stay tuned!</div>;
+//     return null;
+//   }
+
+//   return <News news={news} />;
+// }
 
 export default async function HeroSection() {
   const payload = await getPayloadClient();
@@ -11,10 +32,7 @@ export default async function HeroSection() {
     depth: 1,
   });
 
-  if (!news || news.length === 0) {
-    // return <div className="py-20 text-center">No news yet. Stay tuned!</div>;
-    return null;
-  }
-
   return <News news={news} />;
 }
+
+export const dynamic = 'force-dynamic';

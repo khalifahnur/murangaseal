c53a32eee8950d2051ca2b7f00cab49cf6c2286b
khalifahnur <khalifahnur1095@gmail.com>
2026-03-14 12:18:58 +0300

    ref:added anniversary banner

--- src/components/Banner/AnniversaryBanner.tsx
+++ src/components/Banner/AnniversaryBanner.tsx
@@ -1,6 +1,3 @@
-"use client";
-import React, { useState } from "react";
-import { X } from "lucide-react";
 import Image from "next/image";
 
 type BannerProps = {
@@ -8,17 +5,7 @@ type BannerProps = {
 };
 
 export default function AnniversaryBanner({ variant = "strip" }: BannerProps) {
-  const [isVisible, setIsVisible] = useState(true);
-  const [isClosing, setIsClosing] = useState(false);
 
-  const handleClose = () => {
-    setIsClosing(true);
-    setTimeout(() => {
-      setIsVisible(false);
-    }, 500);
-  };
-
-  if (!isVisible) return null;
 
   const containerClasses =
     variant === "strip"
@@ -26,18 +13,22 @@ export default function AnniversaryBanner({ variant = "strip" }: BannerProps) {
       : "z-50 mx-4 md:mx-auto max-w-6xl mb-12";
 
   return (
-    <div
-      className={`
-        relative transform transition-all duration-500 ease-in-out
-        ${isClosing ? "-translate-y-full opacity-0" : "translate-y-0 opacity-100"}
-        ${containerClasses}
-      `}
-    >
+    <div className={`relative ${containerClasses}`}>
+
+      {/* {windowDimensions.width > 0 && (
+        <Confetti
+          width={windowDimensions.width}
+          height={windowDimensions.height}
+          recycle={false}
+          numberOfPieces={250}
+          style={{ position: 'fixed', top: 0, left: 0, zIndex: 100, pointerEvents: 'none' }}
+        />
+      )} */}
+
       <div className="relative w-full max-h-[100px] md:max-h-[100px] flex justify-center items-center overflow-hidden">
-        
         <div className="w-full h-1/2">
           <Image
-            src="/assets/anniversary-banner.png"
+            src="/assets/anniversary_banner.jpeg"
             alt="Anniversary Celebration"
             width={500} 
             height={200}
@@ -45,22 +36,6 @@ export default function AnniversaryBanner({ variant = "strip" }: BannerProps) {
             priority
           />
         </div>
-        <button
-          onClick={handleClose}
-          className="
-            absolute top-1 right-1 sm:top-2 sm:right-2 md:top-4 md:right-4 z-30
-            p-1.5 md:p-2 
-            rounded-full 
-            bg-white/20 hover:bg-white/40 
-            backdrop-blur-md
-            text-black/60 hover:text-black 
-            transition-all duration-200
-            shadow-sm
-          "
-          aria-label="Dismiss banner"
-        >
-          <X className="w-4 h-4 md:w-6 md:h-6" />
-        </button>
       </div>
     </div>
   );

bcd03114673ff5eb4d5c54928f0a59546ce135f3
khalifahnur <khalifahnur1095@gmail.com>
2026-03-30 14:07:13 +0300

    feat:added anniversary banner

--- src/components/Banner/AnniversaryBanner.tsx
+++ src/components/Banner/AnniversaryBanner.tsx
@@ -15,24 +15,14 @@ export default function AnniversaryBanner({ variant = "strip" }: BannerProps) {
   return (
     <div className={`relative ${containerClasses}`}>
 
-      {/* {windowDimensions.width > 0 && (
-        <Confetti
-          width={windowDimensions.width}
-          height={windowDimensions.height}
-          recycle={false}
-          numberOfPieces={250}
-          style={{ position: 'fixed', top: 0, left: 0, zIndex: 100, pointerEvents: 'none' }}
-        />
-      )} */}
-
       <div className="relative w-full max-h-[100px] md:max-h-[100px] flex justify-center items-center overflow-hidden">
         <div className="w-full h-1/2">
           <Image
             src="/assets/anniversary_banner.jpeg"
             alt="Anniversary Celebration"
             width={500} 
-            height={200}
-            className="w-full h-1/2 object-contain pointer-events-none"
+            height={100}
+            className="w-full h-50 object-contain pointer-events-none"
             priority
           />
         </div>
